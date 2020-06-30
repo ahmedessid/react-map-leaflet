e41f0e321980dce5d180cc0fb39e38e64eb78edc
ahmedessid <ahmed.essidmu@gmail.com>
2020-06-30 12:45:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+
+class Map extends React.Component {
+  render() {
+    return (
+      <LeafletMap
+        center={[34, 12]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <Marker position={[36, 10]}>
+          <Popup>Popup for any custom information.</Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+
+export default Map;
